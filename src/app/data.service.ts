4e6cf2e02c1ddc43ff453ce6bd92637cb7de4ce1
Roof <stephen.roof@bracketglobal.com>
2018-01-31 12:22:24 -0500

    data service improvements

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { cell, gameSession } from './app.module';
+import { cell, gameSession, winningCell } from './app.module';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { collectExternalReferences } from '@angular/compiler/src/output/output_ast';
 
 @Injectable()
 export class DataService {
 
   apiRoot: string = "http://localhost:5000/api";
   results: gameSession;
+  list: number[] = [0, 1, 2];
 
   constructor(private http: HttpClient) { }
 
@@ -18,10 +20,33 @@ export class DataService {
       this.http.get(apiURL).toPromise().then(
         success => {
           this.results = new gameSession();
-          this.results.gameHistory[0] = JSON.parse(success[0].gameState);
-          this.results.gameCount = 3;
-          this.results.aiScore = 1
-          this.results.playerScore = 2;
+          let currentGame: cell[][];
+          let currentCell: cell;
+          let foundWinner: boolean = false;
+
+          for (let record of success as dbRecord[]) {
+
+            foundWinner = false;
+            currentGame = JSON.parse(record.gameState);
+            this.results.gameHistory[this.results.gameCount] = currentGame
+
+            for (let row of this.list) {
+              if (foundWinner == true) break;
+              for (let column of this.list) {
+                if (foundWinner == true) break;
+                currentCell = currentGame[row][column]
+                if (currentCell.isWinner == winningCell.Player) {
+                  this.results.playerScore++;
+                  foundWinner = true;
+                }
+                else if (currentCell.isWinner == winningCell.AI) {
+                  this.results.aiScore++;
+                  foundWinner = true;
+                }
+              }
+            }
+            this.results.gameCount++;
+          }
           resolve();
         }
       )
@@ -33,10 +58,15 @@ export class DataService {
     let apiURL = this.apiRoot + "/TicTacToe";
     let body = JSON.stringify(gameState);
     let headers: HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
-    this.http.post(apiURL, body, { headers }).subscribe(); //not working
+    this.http.post(apiURL, body, { headers }).subscribe();
   }
 
   deleteGameFromDatabase() {
     //code to delete from DB here
   }
+}
+
+class dbRecord {
+  id: number;
+  gameState: string;
 }
