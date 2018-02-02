dad43c17af046ee0a6c4ab3527e035d120ff3d4b
Roof <stephen.roof@bracketglobal.com>
2018-02-02 11:26:17 -0500

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { OuterFrameComponent } from './outer-frame/outer-frame.component';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { GameLogicService } from './game-logic.service';
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/game', pathMatch: 'full' },
+  { path: 'game', component: OuterFrameComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     OuterFrameComponent,
-    GameBoardComponent
+    GameBoardComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [GameLogicService, DataService],
   bootstrap: [AppComponent]
